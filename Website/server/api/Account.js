952a37b6dfa9582910db8706a08012865ce69063
niks-v <51901822+niks-v@users.noreply.github.com>
2023-01-01 16:02:54 +1000

    start on database work

---
+++ Website/server/api/Account.js
@@ -0,0 +1,16 @@
+let Account = () => {
+    this.create = () => {
+
+    }
+    this.login = () => {
+
+    }
+    this.forgotPass = () => {
+
+    }
+    this.delete = () => {
+
+    }
+}
+
+module.exports = Account;
---
+++ Website/server/logic/AccountLogic.js
@@ -0,0 +1,19 @@
+let AccountLogic = (inp) => {
+    this.create = (inp) => {
+
+    }
+    this.login = (inp) => {
+
+    }
+    this.forgotPass = (inp) => {
+
+    }
+    this.delete = (inp) => {
+
+    }
+    this.logout = (inp) => {
+        
+    }
+}
+
+module.exports = AccountLogic;
